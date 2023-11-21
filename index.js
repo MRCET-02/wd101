ebc28fe399190bda20b1a14b74705cee79dd5851
MRCET-02 <121389430+MRCET-02@users.noreply.github.com>
2023-11-21 21:20:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,115 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+ document.addEventListener('DOMContentLoaded', function () {
+            // Load saved data from web storage
+            loadSavedData();
 
-    
-    var dobInput = document.getElementById('dob');
-    var dob = new Date(dobInput.value);
-    var age = new Date().getFullYear() - dob.getFullYear();
+            // Add event listener for form submission
+            document.getElementById('registrationForm').addEventListener('submit', function (event) {
+                event.preventDefault();
+                validateAndSubmitForm();
+            });
 
-    if (age < 18 || age > 55) {
-      alert('Please enter a valid date of birth between 18 and 55 years old.');
-      return;
-    }
+            // Additional validations for the date input field
+            var dobInput = document.getElementById('dob');
+            dobInput.addEventListener('change', function () {
+                validateDateOfBirth();
+            });
+        });
 
-    
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var acceptedTerms = document.getElementById('terms').checked;
+        function validateAndSubmitForm() {
+            // Perform additional validations here
+            if (validateDateOfBirth()) {
+                // If all validations pass, submit the form
+                submitForm();
+            } else {
+                alert('Invalid Date of Birth. Age must be between 18 and 55.');
+            }
+        }
 
-    
-    var userData = { name, email, password, dob: dob.toISOString(), acceptedTerms };
-    var savedData = JSON.parse(localStorage.getItem('userData')) || [];
-    savedData.push(userData);
-    localStorage.setItem('userData', JSON.stringify(savedData));
-    updateTable();
+        function validateDateOfBirth() {
+            // Get the selected date from the input
+            var dobValue = document.getElementById('dob').value;
 
-    this.reset();
-  });
+            // Calculate age based on the selected date
+            var today = new Date();
+            var birthDate = new Date(dobValue);
+            var age = today.getFullYear() - birthDate.getFullYear();
 
-  function updateTable() {
-    var dataTableBody = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
-    dataTableBody.innerHTML = ''; 
+            // Check if the age is between 18 and 55
+            return age >= 18 && age <= 55;
+        }
 
-    var savedData = JSON.parse(localStorage.getItem('userData')) || [];
+        function submitForm() {
+            // Get form data
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+            var dob = document.getElementById('dob').value;
+            var acceptedTerms = document.getElementById('terms').checked;
 
-    savedData.forEach(function(userData) {
-      var row = dataTableBody.insertRow();
+            // Create a new row for the table
+            var newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <td>${name}</td>
+                <td>${email}</td>
+                <td>${password}</td>
+                <td>${dob}</td>
+                <td>${acceptedTerms ? 'Yes' : 'No'}</td>
+            `;
 
-      var nameCell = row.insertCell(0);
-      nameCell.innerHTML = userData.name;
+            // Append the new row to the table
+            document.getElementById('dataTable').getElementsByTagName('tbody')[0].appendChild(newRow);
 
-      var emailCell = row.insertCell(1);
-      emailCell.innerHTML = userData.email;
+            // Save data to web storage
+            saveDataToStorage(name, email, password, dob, acceptedTerms);
 
-      var passwordCell = row.insertCell(2);
-      passwordCell.innerHTML = userData.password;
+            // Clear the form
+            document.getElementById('registrationForm').reset();
+        }
 
-      var dobCell = row.insertCell(3);
-      dobCell.innerHTML = userData.dob;
+        function saveDataToStorage(name, email, password, dob, acceptedTerms) {
+            // Load existing data from web storage
+            var existingData = loadDataFromStorage();
 
-      var termsCell = row.insertCell(4);
-      termsCell.innerHTML = userData.acceptedTerms ? 'Yes' : 'No';
-    });
-};
+            // Add the new entry to the existing data
+            existingData.push({
+                name: name,
+                email: email,
+                password: password,
+                dob: dob,
+                acceptedTerms: acceptedTerms
+            });
+
+            // Convert the object to a JSON string
+            var jsonString = JSON.stringify(existingData);
+
+            // Save the JSON string to web storage
+            localStorage.setItem('userData', jsonString);
+        }
+
+        function loadDataFromStorage() {
+            // Load existing data from web storage
+            var jsonString = localStorage.getItem('userData');
+
+            // Parse the JSON string to an object
+            return jsonString ? JSON.parse(jsonString) : [];
+        }
+
+        function loadSavedData() {
+            // Load data from web storage
+            var existingData = loadDataFromStorage();
+
+            // Display the existing data in the table
+            var tableBody = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+
+            existingData.forEach(function (entry) {
+                var newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${entry.name}</td>
+                    <td>${entry.email}</td>
+                    <td>${entry.password}</td>
+                    <td>${entry.dob}</td>
+                    <td>${entry.acceptedTerms ? 'Yes' : 'No'}</td>
+                `;
+                tableBody.appendChild(newRow);
+            });
+        }
